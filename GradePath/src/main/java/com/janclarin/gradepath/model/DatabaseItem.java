2b9c07147a6a1839a37591107dd1a2ed68b62f9b
janclarin <jervinct@gmail.com>
2014-07-17 01:01:28 -0400

    Fixed day difference logic!

--- GradePath/src/main/java/com/janclarin/gradepath/model/DatabaseItem.java
+++ GradePath/src/main/java/com/janclarin/gradepath/model/DatabaseItem.java
@@ -19,7 +19,7 @@ public abstract class DatabaseItem implements Serializable {
     /**
      * Compares two calendars.
      *
-     * @return number of days apart, return -1 if late.
+     * @return number of days apart.
      */
     protected long getDayDifference(Calendar one, Calendar two) {
         // If they are the same day.
@@ -33,13 +33,28 @@ public abstract class DatabaseItem implements Serializable {
             // TimeUnit.DAYS.convert rounds the day difference.
             // 0 indicates today, -1 indicates yesterday, 1 indicates tomorrow.
             if (dayDifference == 0) {
-                int dateCheck = one.compareTo(two);
-                if (dateCheck == -1) {
-                    return -1;
-                } else if (dateCheck == 1) {
-                    return 1;
-                } else {
+                int dayOne = one.get(Calendar.DAY_OF_MONTH);
+                int dayTwo = two.get(Calendar.DAY_OF_MONTH);
+
+                // If the days are the same, they're on the same day.
+                if (dayOne == dayTwo) {
                     return 0;
+                } else {
+                    int monthOne = one.get(Calendar.MONTH);
+                    int monthTwo = two.get(Calendar.MONTH);
+
+                    // Check months for edge cases like the 31st and 1st of two months.
+                    if (monthOne == monthTwo) {
+                        return dayOne < dayTwo ? 1 : -1;
+
+                        // Check for new years.
+                    } else if (monthOne == 11 && monthTwo == 1) {
+                        return 1;
+                    } else if (monthOne == 0 && monthTwo == 11) {
+                        return -1;
+                    } else {
+                        return monthOne < monthTwo ? 1 : -1;
+                    }
                 }
             } else {
                 return dayDifference;
